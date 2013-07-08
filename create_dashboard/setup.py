fa2735dd2ed6905fbb330585dd39a6ba7bf27349
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-07-08 14:23:41 +0900

    catkinizes turtlebot_create_desktop

--- create_dashboard/setup.py
+++ create_dashboard/setup.py
@@ -1,22 +1,10 @@
-#1/usr/bin/env python
-from setuptools import setup
-import sys
-sys.path.insert(0, 'src')
+#!/usr/bin/env python
 
-setup(name='create_dashboard',
-      version= '0.1.0',
-      packages=['create_dashboard'],
-      package_dir = {'':'src'},
-      install_requires=[],
-      author = "Ze'ev Klapow", 
-      author_email = "zklapow@willowgarage.com",
-      url = "",
-      download_url = "", 
-      keywords = [],
-      classifiers = [
-        "Programming Language :: Python", 
-        "License :: OSI Approved :: BSD License" ],
-      description = "",
-      long_description = "",
-      license = "BSD"
-      )
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(packages=['create_dashboard'],
+                             scripts=['scripts/create_dashboard'],
+                             package_dir={'': 'src'})
+
+setup(**d)
