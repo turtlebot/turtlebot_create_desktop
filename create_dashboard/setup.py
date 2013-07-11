c62dff15beb4d4058adf51576916062cd315543e
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-07-11 10:17:39 +0900

    removes create_dashboard script from global bin

--- create_dashboard/setup.py
+++ create_dashboard/setup.py
@@ -4,7 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(packages=['create_dashboard'],
-                             scripts=['scripts/create_dashboard'],
                              package_dir={'': 'src'})
 
 setup(**d)
