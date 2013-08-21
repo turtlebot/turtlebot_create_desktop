964212af36cb37435d0606b1484cc56a1515dcf0
Marcus Liebhardt <ich@marcusliebhardt.de>
2013-08-21 18:28:56 +0900

    create_dashboard: fixes grey battery status issue (fixes #5)

--- create_dashboard/src/create_dashboard/battery.py
+++ create_dashboard/src/create_dashboard/battery.py
@@ -10,7 +10,21 @@ def non_zero(value):
 
 class TurtlebotBattery(BatteryDashWidget):
     def __init__(self, name='battery'):
-        super(TurtlebotBattery, self).__init__(name)
+        icons = []
+        charge_icons = []
+        icons.append(['ic-battery-0.svg'])
+        icons.append(['ic-battery-20.svg'])
+        icons.append(['ic-battery-40.svg'])
+        icons.append(['ic-battery-60-green.svg'])
+        icons.append(['ic-battery-80-green.svg'])
+        icons.append(['ic-battery-100-green.svg'])
+        charge_icons.append(['ic-battery-charge-0.svg'])
+        charge_icons.append(['ic-battery-charge-20.svg'])
+        charge_icons.append(['ic-battery-charge-40.svg'])
+        charge_icons.append(['ic-battery-charge-60-green.svg'])
+        charge_icons.append(['ic-battery-charge-80-green.svg'])
+        charge_icons.append(['ic-battery-charge-100-green.svg'])
+        super(TurtlebotBattery, self).__init__(name=name, icons=icons, charge_icons=charge_icons)
 
         self._power_consumption = 0.0
         self._pct = 0.0
